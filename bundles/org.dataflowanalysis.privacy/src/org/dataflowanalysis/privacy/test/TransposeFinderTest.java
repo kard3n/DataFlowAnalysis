27ab7ca23a74cb8a40a18473b4efa800e2aa0faa
Jonathan Preiss <jonathan.preiss@yahoo.de>
2026-02-20 17:09:57 +0100

    feat: test for privacy TFG finder

--- bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/test/TransposeFinderTest.java
+++ bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/test/TransposeFinderTest.java
@@ -3,10 +3,15 @@ package org.dataflowanalysis.privacy.test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import org.apache.log4j.Logger;
 import org.dataflowanalysis.analysis.dfd.DFDDataFlowAnalysisBuilder;
 import org.dataflowanalysis.analysis.dfd.core.DFDFlowGraphCollection;
+import org.dataflowanalysis.analysis.utils.LoggerManager;
+import org.dataflowanalysis.dfd.datadictionary.Assignment;
 import org.dataflowanalysis.examplemodels.Activator;
 import org.dataflowanalysis.privacy.PrivacyDFDConfidentialityAnalysis;
 import org.dataflowanalysis.privacy.PrivacyDFDDataFlowAnalysisBuilder;
@@ -19,46 +24,58 @@ import org.junit.jupiter.api.Test;
 
 public class TransposeFinderTest {
 	public static final String TEST_MODEL_PROJECT_NAME = "org.dataflowanalysis.examplemodels";
-	
+
+	private final Logger logger = LoggerManager.getLogger(PrivacyDFDConfidentialityAnalysis.class);
+
 	PrivacyDFDConfidentialityAnalysis analysis;
-	
-	@Test
-	public void shouldSetPropagationResultCorrectly() {
-		// assertDoesNotThrow(() -> vertex.setPropagationResult(incomingCharacteristics,
-		// outgoingCharacteristics, vertexCharacteristics));
 
-		/*
-		 * LoggerManager.getInstance() .setLevel(Level.OFF);
-		 * assertThrowsExactly(IllegalArgumentException.class, () ->
-		 * vertex.setPropagationResult(incomingCharacteristics, outgoingCharacteristics,
-		 * vertexCharacteristics)); LoggerManager.getInstance() .resetLevel();
-		 * 
-		 * assertEquals assertTrue
-		 */
-	}
-	
+	// assertDoesNotThrow(() -> vertex.setPropagationResult(incomingCharacteristics,
+	// outgoingCharacteristics, vertexCharacteristics));
+
+	/*
+	 * LoggerManager.getInstance() .setLevel(Level.OFF);
+	 * assertThrowsExactly(IllegalArgumentException.class, () ->
+	 * vertex.setPropagationResult(incomingCharacteristics, outgoingCharacteristics,
+	 * vertexCharacteristics)); LoggerManager.getInstance() .resetLevel();
+	 * 
+	 * assertEquals assertTrue
+	 */
+
 	@BeforeEach
-    public void initAnalysis() {
-		final var basicDataFlowDiagramPath = Paths.get("models", "dfd", "BasicPrivacy", "basic_privacy.dataflowdiagram");
-        final var basicDataDictionaryPath = Paths.get("models", "dfd", "BasicPrivacy", "basic_privacy.datadictionary");
-        final var basicConsentModelPath = Paths.get("models", "dfd", "BasicPrivacy", "basic_privacy.consentmodel");
-		
-		this.analysis = new PrivacyDFDDataFlowAnalysisBuilder()
-    			.standalone()
-    			.modelProjectName(TEST_MODEL_PROJECT_NAME)
-    			.usePluginActivator(Activator.class)
-    			.useDataFlowDiagram(basicDataFlowDiagramPath.toString())
-    			.useDataDictionary(basicDataDictionaryPath.toString())
-    			.useConsentModel(basicConsentModelPath.toString())
-    			.build();
-    }
+	public void initAnalysis() {
+		final var basicDataFlowDiagramPath = Paths.get("models", "dfd", "SuperBasicPrivacy", "sbp.dataflowdiagram");
+		final var basicDataDictionaryPath = Paths.get("models", "dfd", "SuperBasicPrivacy", "sbp.datadictionary");
+		final var basicConsentModelPath = Paths.get("models", "dfd", "SuperBasicPrivacy", "sbp.consentmodel");
+
+		this.analysis = new PrivacyDFDDataFlowAnalysisBuilder().standalone().modelProjectName(TEST_MODEL_PROJECT_NAME)
+				.usePluginActivator(Activator.class).useDataFlowDiagram(basicDataFlowDiagramPath.toString())
+				.useDataDictionary(basicDataDictionaryPath.toString()).useConsentModel(basicConsentModelPath.toString())
+				.build();
+	}
 
 	@Test
-    public void createsGraphsForRole() {
+	public void createsGraphsForRole() {
 		this.analysis.initializeAnalysis();
-        DFDFlowGraphCollection flowGraph = analysis.findFlowGraphs();
-        assertEquals(flowGraph.getTransposeFlowGraphs()
-                .size(), 2);
-    }
+		DFDFlowGraphCollection flowGraphCollection = analysis.findFlowGraphs();
+		
+
+		flowGraphCollection.evaluate();
+		
+		var flowGraphs = flowGraphCollection.getTransposeFlowGraphs();
+		assertEquals(2, flowGraphs.size());
+
+		
+		// Check that the first graph's sink has the correct data characteristic
+		assert flowGraphs.get(0).getSink().getAllDataCharacteristics().size() == 1;
+		assert flowGraphs.get(0).getSink().getAllDataCharacteristics().get(0).getAllCharacteristics().size() == 2;
+		List<String> labelsOne = List.of("exampleLabel", "RoleLabel");
+		assert flowGraphs.get(0).getSink().getAllDataCharacteristics().get(0).getAllCharacteristics().stream().map(ch -> ch.getValueName()).toList().equals(labelsOne);
+		
+		// Check that the second graph's sink has the correct data characteristic
+		assert flowGraphs.get(1).getSink().getAllDataCharacteristics().size() == 1;
+		assert flowGraphs.get(1).getSink().getAllDataCharacteristics().get(0).getAllCharacteristics().size() == 3;
+		List<String> labelsTwo = List.of("exampleLabel", "BasicConsentLabel", "RoleLabel");
+		assert flowGraphs.get(1).getSink().getAllDataCharacteristics().get(0).getAllCharacteristics().stream().map(ch -> ch.getValueName()).toList().equals(labelsTwo);
+	}
 
 }

bd287ba2e1928a594a3832f3f1a28b038632fd9c
Jonathan Preiss <jonathan.preiss@yahoo.de>
2026-03-19 13:24:08 +0100

    feat: apple wallet privacy case study

---
+++ bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/case_study_scenarios/AppleWallet.java
@@ -0,0 +1,42 @@
+package org.dataflowanalysis.privacy.case_study_scenarios;
+
+import java.nio.file.Paths;
+
+import org.apache.log4j.Logger;
+import org.dataflowanalysis.analysis.dfd.core.DFDFlowGraphCollection;
+import org.dataflowanalysis.analysis.utils.LoggerManager;
+import org.dataflowanalysis.examplemodels.Activator;
+import org.dataflowanalysis.privacy.PrivacyDFDConfidentialityAnalysis;
+import org.dataflowanalysis.privacy.PrivacyDFDDataFlowAnalysisBuilder;
+import org.dataflowanalysis.privacy.constraint.PrivacyDataFlowConstraint;
+import org.dataflowanalysis.privacy.test.PrivacyDataFlowConstrainTest;
+import org.junit.jupiter.api.Test;
+
+public class AppleWallet {
+	public static final String TEST_MODEL_PROJECT_NAME = "org.dataflowanalysis.examplemodels";
+	private static final Logger logger = LoggerManager.getLogger(PrivacyDataFlowConstrainTest.class);
+
+	@Test
+	public void createsGraphsForRole() {
+		final var dataFlowDiagramPath = Paths.get("scenarios", "privacy_dfd", "AppleWallet", "diagram.dataflowdiagram");
+		final var dataDictionaryPath = Paths.get("scenarios", "privacy_dfd", "AppleWallet", "diagram.datadictionary");
+		final var consentModelPath = Paths.get("scenarios", "privacy_dfd", "AppleWallet", "AppleWallet.consentmodel");
+
+		PrivacyDFDConfidentialityAnalysis analysis = new PrivacyDFDDataFlowAnalysisBuilder().standalone()
+				.modelProjectName("org.dataflowanalysis.examplemodels").usePluginActivator(Activator.class)
+				.useDataFlowDiagram(dataFlowDiagramPath.toString()).useDataDictionary(dataDictionaryPath.toString())
+				.useConsentModel(consentModelPath.toString()).build();
+		analysis.initializeAnalysis();
+		DFDFlowGraphCollection flowGraphCollection = analysis.findFlowGraphs();
+		flowGraphCollection.evaluate();
+
+		var violations = PrivacyDataFlowConstraint.findViolations(flowGraphCollection, true);
+
+		logger.info("Detected violations: " + violations.size());
+
+		for (var violation : violations) {
+			logger.info(violation.message());
+		}
+	}
+
+}

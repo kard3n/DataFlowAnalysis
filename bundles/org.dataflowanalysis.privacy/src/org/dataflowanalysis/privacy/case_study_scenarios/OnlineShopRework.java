4f9a018184c555a8db2cece19ec0473cd03ced54
Jonathan Preiss <jonathan.preiss@yahoo.de>
2026-04-02 15:24:12 +0200

    feat: base version of reworked online shop case study

---
+++ bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/case_study_scenarios/OnlineShopRework.java
@@ -0,0 +1,44 @@
+package org.dataflowanalysis.privacy.case_study_scenarios;
+
+import java.nio.file.Paths;
+
+import org.apache.log4j.Logger;
+import org.dataflowanalysis.analysis.dfd.core.DFDFlowGraphCollection;
+import org.dataflowanalysis.analysis.utils.LoggerManager;
+import org.dataflowanalysis.examplemodels.Activator;
+import org.dataflowanalysis.privacy.PrivacyDFDConfidentialityAnalysis;
+import org.dataflowanalysis.privacy.PrivacyDFDDataFlowAnalysisBuilder;
+import org.dataflowanalysis.privacy.constraint.PrivacyDataFlowConstraint;
+import org.dataflowanalysis.privacy.core.PrivacyDFDTransposeFlowGraphFinder;
+import org.dataflowanalysis.privacy.test.PrivacyDataFlowConstrainTest;
+import org.junit.jupiter.api.Test;
+
+public class OnlineShopRework {
+	public static final String TEST_MODEL_PROJECT_NAME = "org.dataflowanalysis.examplemodels";
+	private static final Logger logger = LoggerManager.getLogger(PrivacyDataFlowConstrainTest.class);
+
+	@Test
+	public void createsGraphsForRole() {
+		final var dataFlowDiagramPath = Paths.get("scenarios", "privacy_dfd", "OnlineShopRework", "online_shop.dataflowdiagram");
+		final var dataDictionaryPath = Paths.get("scenarios", "privacy_dfd", "OnlineShopRework", "online_shop.datadictionary");
+		final var consentModelPath = Paths.get("scenarios", "privacy_dfd", "OnlineShopRework", "online_shop.consentmodel");
+
+		PrivacyDFDConfidentialityAnalysis analysis = new PrivacyDFDDataFlowAnalysisBuilder().standalone()
+				.modelProjectName("org.dataflowanalysis.examplemodels").usePluginActivator(Activator.class)
+				.useDataFlowDiagram(dataFlowDiagramPath.toString()).useDataDictionary(dataDictionaryPath.toString())
+				.useConsentModel(consentModelPath.toString()).build();
+		analysis.initializeAnalysis();
+		PrivacyDFDTransposeFlowGraphFinder.setAssigmnentAutoConsentOptions(true);
+		DFDFlowGraphCollection flowGraphCollection = analysis.findFlowGraphs();
+		flowGraphCollection.evaluate();
+
+		var violations = PrivacyDataFlowConstraint.findViolations(flowGraphCollection, true);
+
+		logger.info("Detected violations: " + violations.size());
+
+		for (var violation : violations) {
+			logger.info(violation.message());
+		}
+	}
+
+}

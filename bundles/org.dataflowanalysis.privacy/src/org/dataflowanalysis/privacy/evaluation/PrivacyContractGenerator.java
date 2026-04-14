271973ed37c87b73b91caa1cec982c154466a33e
Jonathan Preiss <jonathan.preiss@yahoo.de>
2026-04-14 15:53:43 +0200

    feat: add generateSourceNodes

--- bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/evaluation/PrivacyContractGenerator.java
+++ bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/evaluation/PrivacyContractGenerator.java
@@ -1,15 +1,25 @@
 package org.dataflowanalysis.privacy.evaluation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.log4j.Logger;
 import org.dataflowanalysis.analysis.utils.LoggerManager;
+import org.dataflowanalysis.dfd.datadictionary.Behavior;
 import org.dataflowanalysis.dfd.datadictionary.DataDictionary;
+import org.dataflowanalysis.dfd.datadictionary.SetAssignment;
+import org.dataflowanalysis.dfd.datadictionary.datadictionaryFactory;
 import org.dataflowanalysis.dfd.dataflowdiagram.DataFlowDiagram;
+import org.dataflowanalysis.dfd.dataflowdiagram.Flow;
 import org.dataflowanalysis.dfd.dataflowdiagram.Node;
+import org.dataflowanalysis.dfd.dataflowdiagram.dataflowdiagramFactory;
 import org.dataflowanalysis.privacy.consentmodel.ConsentModel;
 import org.dataflowanalysis.privacy.consentmodel.ConsentOption;
+import org.dataflowanalysis.privacy.consentmodel.DataItem;
+import org.dataflowanalysis.privacy.consentmodel.DataItemLabel;
+import org.dataflowanalysis.privacy.consentmodel.DataItemLabelType;
 import org.dataflowanalysis.privacy.consentmodel.Role;
 import org.dataflowanalysis.privacy.consentmodel.RoleLabel;
 import org.dataflowanalysis.privacy.consentmodel.consentmodelFactory;
@@ -21,7 +31,9 @@ import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 
 public class PrivacyContractGenerator {
 	private static final Logger logger = LoggerManager.getLogger(PrivacyDataFlowConstrainTest.class);
-	
+	public static Random random = new Random(System.currentTimeMillis());
+	public static String charPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
 	public static PrivacyModelPackage generateUsers(PrivacyModelPackage input, int amount, String sourceNodeName,
 			int maxLabelsPerRole) {
 
@@ -41,21 +53,20 @@ public class PrivacyContractGenerator {
 		if (consentOptions.size() < maxLabelsPerRole) {
 			throw new RuntimeException("The consent model must provide at least maxLabelsPerRole consent options.");
 		}
-		
-		
+
 		HashMap<Integer, Long> numLabelsToUsers = new HashMap<>();
 		for (int i = 0; i <= maxLabelsPerRole; i++) {
 			long previousComb = 0;
-			for(int x = 0; x <=i; x++) {
+			for (int x = 0; x <= i; x++) {
 				previousComb += combinations(i, x);
 			}
 			numLabelsToUsers.put(i, previousComb);
-			
+
 			logger.info(i + ":" + numLabelsToUsers.get(i));
 		}
 
 		int usersAdded = 0;
-		
+
 		for (int i = maxLabelsPerRole; i >= 0; i--) {
 			long numberResultingUsers = numLabelsToUsers.get(i);
 			while (numberResultingUsers + usersAdded <= amount) {
@@ -74,6 +85,92 @@ public class PrivacyContractGenerator {
 			}
 		}
 
+		return input;
+	}
+
+	/**
+	 * 
+	 * @param input                  The input model
+	 * @param sourceNodeName         The name of the node that should be scaled.
+	 *                               Each copy sends a different set of data items.
+	 * @param numberSourceNodes      How many times the node should be replicated
+	 *                               and adapted
+	 * @param dataCombinationSize    The number of labels that each source node
+	 *                               sends
+	 * @param dataCombinationOverlap The number of labels that should overlap
+	 *                               between those sent by two nodes. This is
+	 *                               pair-wise.
+	 * @return The resulting modified models
+	 */
+	public static PrivacyModelPackage generateSourceNodes(PrivacyModelPackage input, String sourceNodeName,
+			int numberSourceNodes, int dataCombinationSize, int dataCombinationOverlap) {
+		var ddFactory = datadictionaryFactory.eINSTANCE;
+		var dfdFactory = dataflowdiagramFactory.eINSTANCE;
+		
+		
+		if (dataCombinationOverlap*2 > dataCombinationSize) {
+			throw new RuntimeException("The requested overlap is bigger than halve the requested combination size.");
+		}
+
+		Node source = null;
+		for (var node : input.dfd().getNodes()) {
+			if (node.getEntityName().equals(sourceNodeName)) {
+				source = node;
+				break;
+			}
+		}
+		if (source == null) {
+			throw new RuntimeException("Could not find the defined source node in the DFD.");
+		}
+
+		final Node sourceCopy = source;
+		Flow sourceOutFlow = input.dfd().getFlows().stream().filter(flow -> flow.getSourceNode() == sourceCopy)
+				.findFirst()
+				.orElseThrow(() -> new RuntimeException("The defined source node does not have an outgoing flow."));
+		
+		if(input.cm().getDataItemLabelType().size() < 1) {
+			new RuntimeException("No DataItemLabelType detected.");
+		}
+		
+		DataItemLabelType labelType = input.cm().getDataItemLabelType().get(0);
+
+		
+		ArrayList<DataItemLabel> overlap = new ArrayList<>();
+		for(int i = 0; i < dataCombinationOverlap; i++) {
+			overlap.add(createUniqueDataItemLabel(labelType));
+		}
+		
+		for(int i = 0; i < numberSourceNodes; i++) {
+			// Create behavior
+			Behavior behavior = ddFactory.createBehavior();
+			behavior.getOutPin().add(sourceOutFlow.getSourcePin());
+			SetAssignment assignment = ddFactory.createSetAssignment();
+			assignment.setOutputPin(sourceOutFlow.getSourcePin());
+			assignment.getOutputLabels().addAll(overlap);
+			overlap.clear();
+			for(int x = 0; x < dataCombinationOverlap; x++) {
+				overlap.add(createUniqueDataItemLabel(labelType));
+			}
+			assignment.getOutputLabels().addAll(overlap);
+			while(assignment.getOutputLabels().size() < dataCombinationSize) {
+				assignment.getOutputLabels().add(createUniqueDataItemLabel(labelType));
+			}
+			
+			// Create node
+			Node newNode = dfdFactory.createExternal();
+			newNode.setEntityName("Node_" + 1);
+			newNode.setBehavior(behavior);
+			
+			//create flow
+			Flow flow = dfdFactory.createFlow();
+			flow.setDestinationNode(sourceOutFlow.getDestinationNode());
+			flow.setDestinationPin(sourceOutFlow.getDestinationPin());
+			flow.setSourceNode(newNode);
+			flow.setSourcePin(sourceOutFlow.getSourcePin());
+			input.dfd().getFlows().add(flow);
+		}
+		
+		
 		return input;
 	}
 
@@ -99,5 +196,30 @@ public class PrivacyContractGenerator {
 		}
 		return result;
 	}
+	
+	/**
+	 * Creates a new unique data item label and associated data item
+	 * @return
+	 */
+	public static DataItemLabel createUniqueDataItemLabel(DataItemLabelType labelType) {
+		String name = generateRandomString(10);
+		DataItemLabel label = consentmodelFactory.eINSTANCE.createDataItemLabel();
+		label.setEntityName(name + "_label");
+		DataItem item = consentmodelFactory.eINSTANCE.createDataItem();
+		item.setEntityName(name);
+		label.setDataItem(item);
+		labelType.getLabels().add(label);
+		return label;
+	}
+	
+	public static String generateRandomString(int length) {
+        StringBuilder randomString = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(charPool.length());
+            randomString.append(charPool.charAt(index));
+        }
+        return randomString.toString();
+    }
 
 }

6c6f12c3b80ce710d5cf059b1d6ba153bc820a67
Jonathan Preiss <jonathan.preiss@yahoo.de>
2026-04-14 21:39:06 +0200

    feat: change base name for user scaled models

--- bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/evaluation/UserScalerTest.java
+++ bundles/org.dataflowanalysis.privacy/src/org/dataflowanalysis/privacy/evaluation/UserScalerTest.java
@@ -26,7 +26,7 @@ public class UserScalerTest {
 				"simple_forward.datadictionary");
 
 		String outputDir = "scaled_models";
-		String scaledModelNameBase = "scaled_";
+		String scaledModelNameBase = "scaled_users_";
 		int numberWarmupRuns = 1; // How many times each scenario should be run before starting the evaluation
 		int numberEvaluationRuns = 5; // How many times each scenario should be ran to determine the result
 		int[] numberUsers = { 1, 100, 1000, 2000, 5000 };
